9bca822cdce99611a6271f9cf5db25f800c587c2
mistertandon <enggparveshtandon@gmail.com>
2018-03-23 14:14:38 +0530

    feat: Implment TouchableHighlight component in App component

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from 'react-native';
+
 import { Ionicons } from '@expo/vector-icons';
 import AddEntry from './components/AddEntry'
+
 export default class App extends Component {
+
+  handlePress = () => {
+    alert('hello from handlePress')
+  }
 
   render() {
     return (
-      <View>
-        <AddEntry />
+      <View style={styles.container}>
+
+        <TouchableHighlight style={styles.btn}
+          onPress={this.handlePress}
+          underlayColor='#d4371b'
+        >
+          <Text style={styles.btnText}>TouchableHighlight Btn</Text>
+        </TouchableHighlight>
+
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      marginLeft: 10,
+      marginRight: 10,
+      justifyContent: 'center'
+    },
+    btn: {
+      backgroundColor: '#E53224',
+      padding: 10,
+      paddingLeft: 50,
+      paddingRight: 50,
+      borderRadius: 5,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    btnText: {
+      color: '#fff'
+    }
+  })
