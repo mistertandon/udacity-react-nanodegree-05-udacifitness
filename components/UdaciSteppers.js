e0d1b13b92d63aa9cfd55892b4f15f1bc740fdc5
mistertandon <enggparveshtandon@gmail.com>
2018-03-26 15:51:20 +0530

    feat: Develop styles for UdaciSteppers component

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,22 +1,52 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 import { FontAwesome, Entypo } from '@expo/vector-icons'
+
+import { purple, gray, white, red, orange, blue, lightPurp, pink } from './../utils/colors'
 
 export default function UdaciSteppers({ value, unit, step, max, onIncrement, onDecrement }) {
 
   return (
-    <View>
 
-      <TouchableOpacity onPress={onDecrement}>
-        <FontAwesome name='minus' size={30} color={'black'} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onIncrement}>
-        <FontAwesome name='plus' size={30} color={'red'} />
-      </TouchableOpacity>
-      <Text>{value}</Text>
-      <Text>{unit}</Text>
+    <View style={styles.stepperContainer}>
+      <View style={styles.flexRowRef}>
+        <TouchableOpacity onPress={onDecrement} style={styles.androidBtn}>
+          <FontAwesome name='minus' size={30} color={'black'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement} style={styles.androidBtn}>
+          <FontAwesome name='plus' size={30} color={'red'} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.matricCounter}>
+        <Text style={{ fontSize: 20 }}>{value}</Text>
+        <Text style={{ color: gray, fontSize: 12 }}>{unit}</Text>
+      </View>
+    </View >
 
-    </View>
   )
 }
+
+const styles = StyleSheet.create(
+  {
+    stepperContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    flexRowRef: {
+      flexDirection: 'row'
+    },
+    androidBtn: {
+      borderWidth: 1,
+      borderRadius: 3,
+      margin: 5,
+      padding: 7
+
+    },
+    matricCounter: {
+      justifyContent: 'center',
+      width:55
+    }
+  }
+)
